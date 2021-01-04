592335cd989f8528298dd47f14485cbda0351823
Amirreza  Sokhankhosh <amirrezasokhankhosh79@gmail.com>
2021-01-04 18:51:06 +0330

    Adding default files and somehow a road map to writing code

---
+++ Tokens.java
@@ -0,0 +1,17 @@
+class Tokens{
+    public static boolean isKeyword(String string){
+        return true; // Just for Compiling correctly.
+    }
+
+    public static boolean isIdentifier(String string){
+        return true; // Just for Compiling correctly.
+    }
+
+    public static boolean isOperator(String string){
+        return true; // Just for Compiling correctly.
+    }
+
+    public static boolean isSeperator(String string){
+        return true; // Just for Compiling correctly.
+    }
+}
